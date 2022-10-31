0b69d76af9d56bdb8f98144fac0242115d8ef189
William Poetra Yoga <william.poetra@gmail.com>
2022-10-31 03:55:38 +0000

    feat: make script more flexible (WIP)

--- nebula/build-deb.sh
+++ nebula/build-deb.sh
@@ -7,28 +7,52 @@ BASEDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 VERSION=1.6.0
 DEBVERSION=2
 
-ARCH=amd64
-DEBIAN_ARCH=amd64
-#ARCH=arm
-#DEBIAN_ARCH=armhf
-#ARCH=arm64
-#DEBIAN_ARCH=arm64
+_ARCH="amd64 arm64"
+
+DEBIAN_ARCH() {
+  case "$1" in
+    amd64) ARCH=amd64;;
+    arm64) ARCH=arm64;;
+  esac
+  echo "$ARCH"
+}
+
+GITHUB_ARCH() {
+  case "$1" in
+    amd64) ARCH=amd64;;
+    arm64) ARCH=arm64;;
+  esac
+  echo "$ARCH"
+}
+
+#echo $(DEBIAN_ARCH amd64)
+#echo $(DEBIAN_ARCH arm64)
+#exit
 
 PKG_NAME=nebula
-PKG_DIR=${PKG_NAME}_${VERSION}-${DEBVERSION}_${DEBIAN_ARCH}
-PKG_FILE=${PKG_DIR}.deb
+FULL_PKG_NAME=${PKG_NAME}_${VERSION}-${DEBVERSION}_$(DEBIAN_ARCH $ARCH)
+PKG_DIR=$FULL_PKG_NAME
+PKG_FILE=$PKG_DIR.deb
 
 GITHUB_ORG=slackhq
 GITHUB_PRJ=nebula
 GITHUB_OS=linux
-GITHUB_ARCH=amd64
 BINPKG_EXT=tar.gz
-BINPKG_FILE=$GITHUB_PRJ-$GITHUB_OS-$GITHUB_ARCH.$BINPKG_EXT
-BINPKG_URL=https://github.com/$GITHUB_ORG/$GITHUB_PRJ/releases/download/v$VERSION/$BINPKG_FILE
-BINPKG_SHA256=f1a82c27624bf319c35f3bb4c136d5b82f26b014e8f5d16f64b3b0089f60220c
+
+_BINPKG_SHA256="f1a82c27624bf319c35f3bb4c136d5b82f26b014e8f5d16f64b3b0089f60220c
+40f926a8c86e2776259db5edf795b904dc8269c33b0d433f1c01e817d8368cd8"
 
 AUX_FILES="AUTHORS CHANGELOG.md LICENSE README.md"
-AUX_FILES_SHA256="2e0c79a5024c1ac2ea8752980bbd4ee8c53297abc583d72f315ad0b5bd4c2e26 da4a6f48fbe5ac8cdf652f6dc6a0625dd583ca52a5ad8357398e1d6980fe8b2e aefd0cce553f24945ce1c692c3c4f9fda581f078ba82977845715cd18565b3bd 1f04b84a30932b4cc33dc9dad93dabeed757cc30b09be6dfb310922b82b24a79"
+AUX_FILES_SHA256="2e0c79a5024c1ac2ea8752980bbd4ee8c53297abc583d72f315ad0b5bd4c2e26
+da4a6f48fbe5ac8cdf652f6dc6a0625dd583ca52a5ad8357398e1d6980fe8b2e
+aefd0cce553f24945ce1c692c3c4f9fda581f078ba82977845715cd18565b3bd
+1f04b84a30932b4cc33dc9dad93dabeed757cc30b09be6dfb310922b82b24a79"
+
+
+
+
+BINPKG_FILE=$GITHUB_PRJ-$GITHUB_OS-$(GITHUB_ARCH $ARCH).$BINPKG_EXT
+BINPKG_URL=https://github.com/$GITHUB_ORG/$GITHUB_PRJ/releases/download/v$VERSION/$BINPKG_FILE
 
 DEPENDENCIES="strip lintian"
 
@@ -65,7 +89,7 @@ install -Dt "${BASEDIR}/${PKG_DIR}/data/usr/share/doc/${PKG_NAME}" -m644 $AUX_FI
 
 cp -r "${BASEDIR}/files/control" "${BASEDIR}/${PKG_DIR}/"
 cp -r "${BASEDIR}/files/debian-binary" "${BASEDIR}/${PKG_DIR}/"
-sed -i "${BASEDIR}/${PKG_DIR}/control/control" -e "s,@VERSION@,${VERSION}-${DEBVERSION}," -e "s,@ARCH@,${DEBIAN_ARCH},"
+sed -i "${BASEDIR}/${PKG_DIR}/control/control" -e "s,@VERSION@,${VERSION}-${DEBVERSION}," -e "s,@ARCH@,$(DEBIAN_ARCH $ARCH),"
 
 (cd "${BASEDIR}/${PKG_DIR}/control" && tar c --owner=0 --group=0 *) | xz -c9 >"${BASEDIR}/${PKG_DIR}/control.tar.xz"
 
